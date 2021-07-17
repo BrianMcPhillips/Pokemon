2293a6c64c8cfcee1bc861166cf170769054846e
Brian McPhillips <B.McPhillips89@gmail.com>
2021-07-16 21:35:46 -0700

    Title added to list item

--- src/components/PokeList/PokeItem/PokeItem.jsx
+++ src/components/PokeList/PokeItem/PokeItem.jsx
@@ -6,6 +6,7 @@ export default class PokeItem extends Component {
     return (
       <div>
        <img src={this.props.pokeData.url_image} alt={this.props.pokeData.pokemon} />
+       <h3>{this.props.pokeData.pokemon}</h3>
       </div>
     )
   }
