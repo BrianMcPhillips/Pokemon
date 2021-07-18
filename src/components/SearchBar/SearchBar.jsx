31985e1e100218356b5bf00a0f9fd5c918ef3ac4
Brian McPhillips <B.McPhillips89@gmail.com>
2021-07-18 16:39:32 -0700

    Search function and fetch button working

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -4,11 +4,13 @@ export default class SearchBar extends Component {
   render() {
     return (
       <div>
-        <select onChange={this.props.handle} >
+        <select onChange={this.props.handleOption} >
           {
             this.props.data.map(booger => <option key={booger} value={booger}>{booger}</option>)
           }
         </select>
+        <input onChange={this.props.handleName} type="text"></input>
+        <button onClick={this.props.handleClick}>Fetch Pokemon</button>
       </div>
     )
   }
