f3e9a80d98bd96b3ed8873a7491fb4245759512f
Brian McPhillips <B.McPhillips89@gmail.com>
2021-08-07 23:29:24 -0700

    Update PokeList.jsx

--- src/components/SearchPage/PokeList/PokeList.jsx
+++ src/components/SearchPage/PokeList/PokeList.jsx
@@ -1,13 +1,16 @@
+//Import Libraries and Components
 import React, { Component } from 'react';
 import styles from './PokeList.module.css';
 import PokeItem from './PokeItem/PokeItem';
 
 export default class PokeList extends Component {
   render() {
+    //Destructure Props
     const { data, prev, next, pageNum, totalPage } = this.props;
     return (
       <div className={styles.body}>
         <h3>Pokemon List</h3>
+        //Map over data to pass props to individual character components and set them to state
         <div className={styles.list}>
           {
             data.map(poke => <PokeItem key={poke.pokemon} pokeData={poke}/>)
